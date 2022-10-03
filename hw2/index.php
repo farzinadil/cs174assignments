2a65764560497faf9c72b2a41d16927c9cc5e51f
Farzin Adil <54300496+farzinadil@users.noreply.github.com>
2022-10-03 00:32:10 -0700

    changes

--- hw2/index.php
+++ hw2/index.php
@@ -1,8 +1,85 @@
+<!DOCTYPE html>
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- </body>
+<body>
+
+<h1>Pizza Place</h1>
+
+<?php
+function toppings() {
+    echo "<h1>Toppings</h1>";
+    $toppings = array("Pepperoni", "Cheese", "Hawaiian", "Meat Lovers", "Veggie");
+    echo "<form action='index.php' method='post'>";
+    for ($i = 0; $i < 5; $i++) {
+        echo "<input type='checkbox' name='topping' value='" . $toppings[$i] . "'>" . $toppings[$i] . "<br>";
+    }
+    echo "<input type='submit' name='page' value='detail'>";
+    echo "</form>";
+}
+function menu() {
+    echo "<h2>Menu</h2>";
+    $pizza = array("Pepperoni", "Cheese", "Hawaiian", "Meat Lovers", "Veggie");
+    $price = array(10, 8, 12, 14, 11);
+    $popularity = array(1, 2, 3, 4, 5);
+    $order = array("Order", "Order", "Order", "Order", "Order");
+    echo "<table border='1'>
+    <tr>
+    <th>Pizza</th>
+    <th>Price</th>
+    <th>Popularity</th>
+    <th>Order</th>
+    </tr>";
+    for ($i = 0; $i < 5; $i++) {
+        echo "<tr>";
+        echo "<td>" . $pizza[$i] . "</td>";
+        echo "<td>" . $price[$i] . "</td>";
+        echo "<td>" . $popularity[$i] . "</td>";
+        echo "<td>" . $order[$i] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+    // order button 
+    echo "<form action='index.php' method='post'>";
+    echo "<input type='submit' label='order' name='page' value='toppings'>";
+    echo "</form>";
+
+    
+  }
+
+  //function to list details of pizzed selected
+    function detail() {
+        echo "<h1>Detail</h1>";
+        //print toppings selected
+        echo "<h2>Toppings</h2>";
+        echo "<ul>";
+        echo "<li>" . $_POST['topping'] . "</li>";
+        echo "</ul>";
+      
+    }
+
+$page = $_POST['page'];
+$topping = $_POST['topping'];
+$pst = $_POST;
+
+
+
+
+
+switch ($page) {
+    case "detail":
+        echo detail();
+        break;
+    case "toppings":
+        echo toppings();
+        break;
+    default:
+        echo menu();
+}
+
+
+
+
+
+?> 
+
+</body>
 </html>
---
+++ hw2/save-index.php
@@ -0,0 +1,51 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php 
+ // piza menu with pizza, prices, popularity, and order button
+ function menu() {
+    echo "<h1>Menu</h1>";
+    $pizza = array("Pepperoni", "Cheese", "Hawaiian", "Meat Lovers", "Veggie");
+    $price = array(10, 8, 12, 14, 11);
+    $popularity = array(1, 2, 3, 4, 5);
+    $order = array("Order", "Order", "Order", "Order", "Order");
+    echo "<table border='1'>
+    <tr>
+    <th>Pizza</th>
+    <th>Price</th>
+    <th>Popularity</th>
+    <th>Order</th>
+    </tr>";
+    for ($i = 0; $i < 5; $i++) {
+        echo "<tr>";
+        echo "<td>" . $pizza[$i] . "</td>";
+        echo "<td>" . $price[$i] . "</td>";
+        echo "<td>" . $popularity[$i] . "</td>";
+        echo "<td>" . $order[$i] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+    
+    
+  }
+  // pizza toppings selection with checkboxes
+    function toppings() {
+    echo "<h1>Toppings</h1>";
+    $toppings = array("Pepperoni", "Cheese", "Hawaiian", "Meat Lovers", "Veggie");
+    echo "<form action='index.php' method='post'>";
+    for ($i = 0; $i < 5; $i++) {
+        echo "<input type='checkbox' name='toppings[]' value='" . $toppings[$i] . "'>" . $toppings[$i] . "<br>";
+    }
+    echo "<input type='submit' name='submit' value='Submit'>";
+    echo "</form>";
+    }
+
+
+    echo menu(); 
+    echo toppings();
+
+ ?> 
+ </body>
+</html>
