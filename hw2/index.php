f32d0f5f9bd9419a036282bb5d03ce6e8f3db009
jeffyang400 <56237675+jeffyang400@users.noreply.github.com>
2022-10-06 16:23:32 -0700

    Alt Version
    Code based more on what the profs wants

--- hw2/index.php
+++ hw2/index.php
@@ -1,156 +1,133 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1 style=color:SlateBlue><center><u>Original Pizza Place<u></center></h1>
 
 <?php
+const PIE_FILE = "Pizzas.txt";
+$activity = (isset($_REQUEST['a']) && in_array($_REQUEST['a'],
+        ["menu", "edit", "detail", "delete"])) ? $_REQUEST['a'] . "Controller" : "menuController";
+$activity();
 
-//Main list of pizzas created
-function menu() {
-    echo "<h2><center>Menu<center></h2>";
-    $pizza = array("Hawaiian", "Meat Lovers", "Veggie");
-    $price = array(12, 14, 11);
-    $popularity = array(4, 4, 5);
-    $actions = array("Order", "Order", "Order");
-    echo "<center><table border='0'>
-    <tr>
-    <th>Pizza</th>
-    <th>Price</th>
-    <th>Popularity</th>
-    <th>Actions</th>
-    </tr>";
-    echo "<form action='index.php' method='post'>";
-    for ($i = 0; $i < sizeof($pizza); $i++) {
-        echo "<tr>";
-        echo "<td style=color:SlateBlue><u>" . $pizza[$i] . "</u></td>";
-        echo "<td>$" . $price[$i] . "</td>";
-        echo "<td>";
-        for ($j = 0; $j < $popularity[$i]; $j++) {
-            echo "💗";
+function menuController() {
+    $pizza["PIE_FILE"] = getEntries();
+    $pizza["PIE_FILE"] = processNewPie($pizza["PIE_FILE"]);
+    $layout = (isset($_REQUEST['f']) && $_REQUEST['f'] == "html") ? $_REQUEST['f'] . "Layout" : "htmlLayout";
+    $layout($pizza, "menuView");
+}
+
+function getEntries() {
+    if (file_exists(PIE_FILE)) {
+        $entries = unserialize(file_get_contents(PIE_FILE));
+        if ($entries) {
+            return $entries;
         }
-        echo "</td>";
-        //Unable to go to the delete page when clicking deleting button
-        echo "<td>" . "<input type='submit' name='page' value='Edit'>" .
-            "<input type='submit' name='page' value='Delete'>" . "</td>";
-        echo "</tr>";
     }
-    echo "</table></center>";
-    echo "<center><input type='submit' label='order' name='page' value='Add Pie'></center>";
-    echo "</form>";
+    return [];
 }
 
-//Enter Pizza create/edit mode with given amount of toppings
-function toppings() {
-    echo "<h2>Pie Editor</h2>";
-    //Need to add functionality to save pizza name and price into menu
-    $toppings = array("Pepperoni", "Cheese", "Green pepper", "Pineapple", "Olives");
-    echo "<form action='index.php' method='post'>";
-    echo "<input size='25' type='text' name='pizza' label='Pizza Name' placeholder='Enter Pizza Name'>" .    
-    "<input size='4' type='text' name='price' label='Pizza Price' placeholder='Price'>" . 
-    "<br><br>";
-    for ($i = 0; $i < 5; $i++) {
-        echo "<input type='checkbox' name='topping[]' value='" . $toppings[$i] . "'>" . $toppings[$i] . "<br>";
+function processNewPie($pizza) {
+    $name = (isset($_REQUEST['name'])) ?
+        filter_var($_REQUEST['name'], FILTER_SANITIZE_STRING) : "";
+    $price = (isset($_REQUEST['price'])) ?
+        filter_var($_REQUEST['price'], FILTER_SANITIZE_NUMBER_INT) : "";
+    if ($name == "" || $price == "") {
+        return $pizza;
     }
-    echo "<input type='submit' name='page' value='Create'>";
-    echo "</form>";
+    $pizza = array_merge([$name => $price], $pizza);
+    file_put_contents(PIE_FILE, serialize($pizza));
+    return $pizza;
+}
 
+function editController() {
+    $pizza["NAME"] = (isset($_REQUEST['name'])) ?
+        filter_var($_REQUEST['name'], FILTER_SANITIZE_STRING) : "";
+    $entries = getEntries();
+    $layout = (isset($_REQUEST['f']) && $_REQUEST['f'] == "html") ? $_REQUEST['f'] . "Layout" : "htmlLayout";
+    $layout($pizza, "editView");
+}
+
+function detailController() {
 
 }
 
-//function to list details of pizza selected
-function detail() {
-    echo "<h2>Detail</h2>";
-    //pizza name from post
-    $pizza = $_POST['pizza'];
-    echo "<h3>Pizza Name</h3>";
-    echo "<p>" . $pizza . "</p>";
-    echo "<h3>Price</h3>";
-    //price from post
-    $price = $_POST['price'];
-    echo "<p>$" . $price . "</p>";
-    
+function deleteController() {
 
-  
+}
 
-    //draw pizza
-    echo "<svg height='100' width='100'>
-    <circle cx='50' cy='50' r='40' stroke='black' stroke-width='3' fill='red' />
-    </svg>";
-    //print selected toppings from post
-    echo "<h3>Toppings</h3>";
-    echo "<ul>";
-    foreach ($_POST['topping'] as $topping) {
-        echo "<li>" . $topping . "</li>";
+function htmlLayout($pizza, $view) {
+    ?><!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            h1 {text-align: center;}
+            h2 {text-align: center;}
+        </style>
+        <title>Original Pizza Place <?php if (!empty($pizza['NAME'])) {
+        echo ":" . $pizza['NAME'];
+    } ?></title>
+    </head>
+    <body>
+    <?php
+    $view($pizza);
+    ?>
+    </body>
+</html><?php
+}
+
+function menuView($pizza) {
+    ?>
+    <h1><a href="index.php">Original Pizza Place</a></h1>
+    <h2>Menu</h2>
+    <form>
+    <div>
+        <table>
+            <tr>
+                <th>Pizza</th>
+                <th>Price</th>
+                <th>Popularity</th>
+                <th>Actions</th>
+            </tr>
+            <?php
+            if (!empty($pizza)) {
+                foreach($pizza["PIE_FILE"] as $name => $price) {
+                    ?><tr>
+                        <td><a href="index.php?a=detail&name=<?=urlencode($name)?>">
+                                <?=$name ?></a></td>
+                        <td>$<?=$price ?></td>
+                        <td>💗</td>
+                        <td>
+                            <button><a href="index.php?a=edit&name=<?=urlencode($name)?>">Edit</a></button>
+                            <button><a href="index.php?a=delete&name=<?=urlencode($name)?>">Delete</a></button>
+                        </td>
+                    </tr><?php
+                 }
+            }?>
+        </table>
+    </div>
+        <button><a href="index.php?a=edit">Add Pie</a></button><?php
+}
+
+function editView($pizza) {
+    $toppings = array("Red Sauce", "Mozzarella", "Pepperoni", "Pineapple",
+        "Green Peppers", "Ham", "Mushrooms", "Anchovies");
+    ?>
+    <h1><a href="index.php">Original Pizza Place</a></h1>
+    <h2>Pie Editor</h2>
+    <form>
+    <input size='25' type='text' name='name' placeholder='Enter Pizza Name'>
+        <input size='4' type='text' name='price' placeholder='Price'>
+    <h3>Toppings: </h3>
+    <div>
+    <?php
+    foreach ($toppings as $topping) {
+        echo "<input type='checkbox' name='topping[]' value='" . $topping . "'>" . $topping . "<br>";
     }
-    echo "</ul>";
+    ?></div>
+    <button>Create</button></form><?php
+}
 
-    //back button to return to menu
-    echo "<form action='index.php' method='post'>";
-    echo "<input type='submit'>";
-    echo "</form>";
-
-   
-
-
+function deleteView($data) {
+       ?> <?php
 
 }
 
-function delete() {
-    //Need to add given pizza name
-    echo "Are you sure you want to delete the bookmark: (INSERT PIZZA NAME)?";
-    echo "<form action='index.php' method='post'>";
-    echo "<input type='submit' name='page' value='Confirm'>";
-    echo "<input type='submit' name='page' value='Cancel'>";
-    echo "</form>";
-
-    //print post 
-    echo "<pre>";
-    print_r($_POST);
-    echo "</pre>";
-
-
-  
+function detailView($data) {
+        ?> <?php
 }
-
-$page = $_POST['page'];
-
-
-
-
-
-
-switch ($page) {
-    case "Create":
-        echo detail();
-        break;
-    case "Add Pie":
-        echo toppings();
-        break;
-    case "Edit":
-        // call toppings function passing in pizza name, toppings, and price as parameters
-        // ex: toppings($pizza, $toppings[], $price);
-        echo toppings();
-        break;
-    case "Delete":
-        echo delete();
-        break;
-    case "Confirm":
-        // call function to delete pizza from filw
-        echo menu();
-        break;
-    case "Cancel":
-        echo menu();
-        break;
-    default:
-        echo menu();
-}
-
-
-
-
-
-?>
-
-</body>
-</html>
