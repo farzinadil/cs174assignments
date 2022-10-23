4b43227b54654f6434cb1d0eda1552dfc6f3548f
Farzin <farzinadil@gmail.com>
2022-10-23 00:26:52 -0700

    database

--- hw3/src/configs/CreateDB.php
+++ hw3/src/configs/CreateDB.php
@@ -1,1 +1,34 @@
 <?php
+// create mysql database with php table policy wuth fields title, agent email, duration, description
+
+// connect to mysql
+$server_name = "localhost";
+$user_name = "root";
+$password = "";
+$connection = mysqli_connect($server_name, $user_name, $password);
+
+// check connection
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+// create database
+$sql = "CREATE DATABASE insurance";
+if (mysqli_query($connection, $sql)) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . mysqli_error($connection);
+}
+
+// create table
+$sql = "CREATE TABLE policy (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(30) NOT NULL,
+agent_email VARCHAR(50),
+duration VARCHAR(50),
+description VARCHAR(50)
+)";
+
+
+?>
