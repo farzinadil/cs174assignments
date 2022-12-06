e2208297c0b4714154187a9c015d75aab39543a4
Farzin <farzinadil@gmail.com>
2022-12-05 23:02:01 -0800

    home page

--- hw5/client/src/components/HomePage.js
+++ hw5/client/src/components/HomePage.js
@@ -1,13 +1,30 @@
-import React from "react";
-import { Link } from 'react-router-dom'
-
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const HomePage = () => {
-    return (
-        <div>
-        <h1>Home Page</h1>
-        </div>
-    );
-}
+  const [suggestedDate, setSuggestedDate] = useState('');
+
+  useEffect(() => {
+    axios.get('localhost:8888/match')
+      .then((response) => {
+        setSuggestedDate(response.data.name);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Suggested Date</h1>
+      <p>{suggestedDate}</p>
+      <Link to="/Login"> <button>Sign Out</button></Link>
+    </div>
+  );
+};
 
 export default HomePage;
+
+
+
